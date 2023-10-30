d1780c8426e27999045550cb6809990e2eaa1421
Gui <guitimliu@gmail.com>
2023-10-30 23:56:19 +0800

    style: 安裝與測試SCSS 環境 & 加入 CSS Reset

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,12 @@
     <p class="home__content">Hello World!</p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .home {
+    &__content {
+      font-size: 20px;
+      line-height: 1.2;
+    }
+  }
+</style>
