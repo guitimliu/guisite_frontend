ec6f632f311e1a2e6b8186f0930362ff6f1d69dd
Gui <guitimliu@gmail.com>
2023-10-30 23:47:04 +0800

    feat: init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Home from './pages/index.vue';
+</script>
+
+<template>
+  <div>
+    <Home />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <p class="home__content">Hello World!</p>
+  </div>
+</template>
