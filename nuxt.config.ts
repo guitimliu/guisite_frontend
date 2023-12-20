a84178352aebacda9bfedd0ee36a9cdace71315d
Gui <guitimliu@gmail.com>
2023-12-21 01:31:49 +0800

    style: Mixin & 基本樣式設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/scss/global.scss']
+  css: ['~/scss/global.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/scss/mixin.scss" as *;',
+        },
+      },
+    },
+  },
 })
