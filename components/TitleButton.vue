25f2c1ad2250de7de3be7c1561ed934592671b2e
Gui <guitimliu@gmail.com>
2023-12-21 01:40:38 +0800

    feat: TitleButton 標題按鈕元件完成

---
+++ components/TitleButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: '標題',
+  }
+})
+</script>
+
+<template>
+  <div class="titleButton">
+    {{ title }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.titleButton {
+  display: inline-block;
+  border: 1px solid #111111;
+  border-radius: 12px;
+  padding: 12px 20px;
+
+  @include desktop-up() {
+    transform: rotate(90deg);
+  }
+}
+</style>
